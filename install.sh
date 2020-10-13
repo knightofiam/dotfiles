514c15237e9fe868c026f8cd5adb1cdfe01a07e3
Aaron Mahan <knightofiam@gmail.com>
2020-10-13 00:38:11 -0600

    Improve Homebrew installation.
    - Refactor the test for Homebrew installation for simplicity.
    
    - Update Homebrew regardless of whether or not it had to be installed
      first.

--- install.sh
+++ install.sh
@@ -20,17 +20,17 @@ printf "\nFinished symlinking dotfiles.\n\n"
 cd ${THIS_DIR}
 
 printf "Checking if Homebrew is installed...\n\n"
-which -s brew
-if [[ $? != 0 ]] ; then
+if test ! $(which brew); then
   printf "Installing Homebrew...\n\n"
   bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   printf "\nFinished installing Homebrew.\n\n"
 else
-  printf "Homebrew is already installed.\n"
-  printf "Updating Homebrew...\n\n"
-  brew update
-  printf "Finished updating Homebrew\n\n"
+  printf "Homebrew is already installed.\n\n"
 fi
+
+printf "Updating Homebrew...\n\n"
+brew update
+printf "Finished updating Homebrew.\n\n"
 
 printf "Installing Homebrew formulae using Brewfile...\n\n"
 brew tap homebrew/bundle
