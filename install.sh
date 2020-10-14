d02fe608db26b5063745ddd8e13131fc59babd56
Aaron Mahan <knightofiam@gmail.com>
2020-10-13 20:52:02 -0600

    Symlink MacVim.app to /Applications.
    Homebrew's cask install of MacVim fails because vim is
    already installed. The workaround is to install the non-cask
    version of MacVim, and then manually symlink the MacVim.app
    to /Applications.
    
    For file associations - such as always opening .txt files
    with MacVim - the actual application rather than the symlink
    must be used, e.g.:
    
      /usr/local/Cellar/macvim/8.2-166_1/MacVim.app.

--- install.sh
+++ install.sh
@@ -18,6 +18,11 @@ brew tap homebrew/bundle
 brew bundle
 printf "\nFinished installing Homebrew formulae using Brewfile.\n\n"
 
+
+printf "Symlinking MacVim.app to /Applications...\n\n"
+ln -sfh /usr/local/Cellar/macvim/8.2-166_1/MacVim.app /Applications/
+printf "Finished symlinking MacVim.app to /Applications.\n\n"
+
 printf "Symlinking dotfiles to ${HOME}...\n\n"
 THIS_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 dotfiles=("bash_profile" "cvimrc" "git/gitconfig" "git/gitignore_global" "vim" "vim/vimrc")
