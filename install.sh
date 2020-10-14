6201129065fe43f0f7cac36d2ac5fc6834bee430
Aaron Mahan <knightofiam@gmail.com>
2020-10-13 20:32:03 -0600

    Symlink dotfiles *after* installing from Brewfile.
    - This is safer in case the realpath command isn't installed, which is
      used for symlinking dotfiles from the script directory. The Brewfile
      installs coreutils, which installs a copy of realpath.
    
    - Add some words from this commit message to the vim spellfile.

--- install.sh
+++ install.sh
@@ -1,21 +1,5 @@
 #!/usr/bin/env bash
 
-THIS_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
-dotfiles=("bash_profile" "cvimrc" "git/gitconfig" "git/gitignore_global" "vim" "vim/vimrc")
-
-cd ${HOME}
-
-printf "\nSymlinking dotfiles to ${HOME}...\n\n"
-for dotfilePath in "${dotfiles[@]}"
-do
-  dotfileName=$(basename "${dotfilePath%.*}")
-  printf "Symlinking ${THIS_DIR}/${dotfilePath} as ${HOME}/.${dotfileName}\n"
-  ln -sfh ${THIS_DIR}/${dotfilePath} ".${dotfileName}"
-done
-printf "\nFinished symlinking dotfiles.\n\n"
-
-cd ${THIS_DIR}
-
 printf "Checking if Homebrew is installed...\n\n"
 if test ! $(which brew); then
   printf "Installing Homebrew...\n\n"
@@ -33,3 +17,16 @@ printf "Installing Homebrew formulae using Brewfile...\n\n"
 brew tap homebrew/bundle
 brew bundle
 printf "\nFinished installing Homebrew formulae using Brewfile.\n\n"
+
+printf "Symlinking dotfiles to ${HOME}...\n\n"
+THIS_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+dotfiles=("bash_profile" "cvimrc" "git/gitconfig" "git/gitignore_global" "vim" "vim/vimrc")
+cd ${HOME}
+for dotfilePath in "${dotfiles[@]}"
+do
+  dotfileName=$(basename "${dotfilePath%.*}")
+  printf "Symlinking ${THIS_DIR}/${dotfilePath} as ${HOME}/.${dotfileName}\n"
+  ln -sfh ${THIS_DIR}/${dotfilePath} ".${dotfileName}"
+done
+printf "\nFinished symlinking dotfiles.\n\n"
+
