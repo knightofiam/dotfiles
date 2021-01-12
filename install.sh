bf015d661a1bf7f399e3e34a4b0b180c62104f3d
Aaron Mahan <knightofiam@gmail.com>
2021-01-12 11:09:31 -0700

    Configure git repos, including dotfiles.
    - Comes after installing / upgrading git via Homebrew.
    
    - Allows downloading / installing dotfiles even without git previously
      installed.
    
    - Also set up game dev repo.

--- install.sh
+++ install.sh
@@ -50,6 +50,25 @@ if [[ $(grep -L "/usr/local/bin/bash" /etc/shells) ]]; then
   chsh -s /usr/local/bin/bash
 fi
 
+# Configure dotfiles git repository.
+# Preserve any local changes.
+cd ${THIS_DIR}
+if [[ ! $(git rev-parse --is-inside-work-tree 2>/dev/null) ]]; then
+  git init
+  git config remote.origin.url >/dev/null 2>&1 || \
+    git remote add origin https://github.com/knightofiam/dotfiles.git
+  git fetch
+  git reset origin/master
+  git branch --set-upstream-to=origin/master
+  git ls-files -z --deleted | xargs -0 git checkout --
+  git submodule update --init --recursive
+fi
+
+# Configure GitHub development projects, including dotfiles.
+mkdir -p "${HOME}/projects/godot"
+mv "${HOME}/dotfiles" "${HOME}/projects/"
+git clone https://github.com/forerunnergames/coa.git "${HOME}/projects/godot/coa"
+
 # Configure file associations.
 duti duti
 
