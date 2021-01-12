920df2df02d91737a946d10e558309f3ec6ced39
Aaron Mahan <knightofiam@gmail.com>
2021-01-12 11:05:31 -0700

    Backup any existing ~/.vim during installation.
    - A ~/.vim directory gets auto-created by vimrc when running vim, which
      causes problems during installation of actual ~/.vim.

--- install.sh
+++ install.sh
@@ -27,6 +27,15 @@ printf "Symlinking dotfiles to ${HOME}...\n\n"
 THIS_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 dotfiles=("bash_profile" "cvimrc" "duti" "git/gitconfig" "git/gitignore_global" "ideavimrc" "macos" "vim" "vim/vimrc")
 cd ${HOME}
+# ~/.vim directory gets auto-created by vimrc
+# Back it up if it's not a symlink.
+if [[ -d .vim && ! -L .vim ]]; then
+  printf "${HOME}/.vim already exists, backing up before overwriting...\n"
+  # Using gmv from coreutils for backup feature.
+  # Must come after `brew bundle` (installs coreutils).
+  /usr/local/bin/gmv --backup=numbered ".vim" ".vim.backup"
+  printf "\nFinished backing up ${HOME}/.vim."
+fi
 for dotfilePath in "${dotfiles[@]}"
 do
   dotfileName=$(basename "${dotfilePath}")
