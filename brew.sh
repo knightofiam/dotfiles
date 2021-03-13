1cb3e2fb54fbd6d2df2e4f28d46b3912bf0b8ea9
Aaron Mahan <knightofiam@gmail.com>
2021-03-12 18:01:16 -0700

    Don't prompt during homebrew installation.

--- brew.sh
+++ brew.sh
@@ -3,7 +3,7 @@
 printf "Checking if Homebrew is installed...\n\n"
 if test ! $(which brew); then
   printf "Installing Homebrew...\n\n"
-  bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   printf "\nFinished installing Homebrew.\n\n"
 else
   printf "Homebrew is already installed.\n\n"
