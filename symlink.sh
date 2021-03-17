61575ec8365cb4addec14298246eccaa773a25a3
Aaron Mahan <knightofiam@gmail.com>
2021-03-16 22:53:09 -0600

    Add ~/.ssh dotfile directory.
    - Exclude ~/.ssh/known_hosts; only add the configuration.
    - Configure ssh sessions to stay connected for an hour.

--- symlink.sh
+++ symlink.sh
@@ -3,40 +3,38 @@
 printf "Symlinking dotfiles to ${HOME}...\n\n"
 
 THIS_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
-dotfiles=("bash_profile" "cvimrc" "duti" "git/gitconfig" "git/gitignore_global" "ideavimrc" "vim" "vim/vimrc")
+dotfiles=("bash_profile" "cvimrc" "duti" "git/gitconfig" "git/gitignore_global" "ideavimrc" "ssh" "vim" "vim/vimrc")
 
 cd ${HOME}
-
-# ~/.vim directory gets auto-created by vimrc
-# Back it up if it's not a symlink.
-if [[ -d .vim && ! -L .vim ]]; then
-  printf "${HOME}/.vim already exists, backing up before overwriting...\n"
-  # Using gmv from coreutils for backup feature.
-  # Must come after brew.sh (installs coreutils).
-  /usr/local/bin/gmv --backup=numbered ".vim" ".vim.backup"
-  printf "\nFinished backing up ${HOME}/.vim."
-fi
 
 for dotfilePath in "${dotfiles[@]}"
 do
   dotfileName=$(basename "${dotfilePath}")
-
-  if [[ ( -f ${HOME}/.${dotfileName} || -d ${HOME}/.${dotfileName} ) && ! -L ${HOME}/.${dotfileName} ]]; then
-    printf "WARNING: ${HOME}/.${dotfileName} exists and is not a symlink, skipping...\n"
-    continue
-  fi
 
   if [[ ! -f ${THIS_DIR}/${dotfilePath} && ! -d ${THIS_DIR}/${dotfilePath} ]]; then
     printf "WARNING: ${THIS_DIR}/${dotfilePath} doesn't exist, skipping...\n"
     continue
   fi
 
+  # Back up existing dotfiles before replacing if they're not symlinks.
+  if [[ ( -f ${HOME}/.${dotfileName} || -d ${HOME}/.${dotfileName} ) && ! -L ${HOME}/.${dotfileName} ]]; then
+    printf "${HOME}/.${dotfileName} already exists, backing up before overwriting...\n"
+    # Using gmv from coreutils for backup feature.
+    # Must come after brew.sh (installs coreutils).
+    /usr/local/bin/gmv --backup=numbered "${HOME}/.${dotfileName}" "${HOME}/.${dotfileName}.backup"
+    printf "Finished backing up ${HOME}/.${dotfileName}.\n"
+  fi
+
   printf "Symlinking ${THIS_DIR}/${dotfilePath} as ${HOME}/.${dotfileName}\n"
-  ln -sfn ${THIS_DIR}/${dotfilePath} ".${dotfileName}"
+  ln -sfn ${THIS_DIR}/${dotfilePath} "${HOME}/.${dotfileName}"
 done
 
+# TODO Combine with above loop by resolving symlink path for dotfiles/.extra -> ~/Sync/dev/.extra
+#         - Check if is symlink
+#         - If is symlink, use symlinked path (actual_path_of (${THIS_DIR}/dotfile)).
+#         - Othwerwise, proceed as usual (assume path is ${THIS_DIR}/dotfile)
 # Symlink ~/.extra for local settings.
-if [[ ! -f .extra && -f ${HOME}/Sync/dev/.extra ]]; then
+if [[ ! -f ${HOME}/.extra && -f ${HOME}/Sync/dev/.extra ]]; then
   printf "Symlinking ${HOME}/Sync/dev/.extra as ${HOME}/.extra\n"
   ln -sfh ${HOME}/Sync/dev/.extra .extra
 elif [[ -f .extra ]]; then
