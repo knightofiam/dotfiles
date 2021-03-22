ff8c4a2cc68b13a922a4b8d8d951543d9d7061ff
Aaron Mahan <knightofiam@gmail.com>
2021-03-22 15:33:39 -0600

    Improve symlink.sh.
    - Skip existing dotfile if gmv command doesn't exist, since it can't be
      backed up in that case.
    
    - DRY: Extract constants and local variables to remove repetition.

--- symlink.sh
+++ symlink.sh
@@ -1,30 +1,38 @@
 #!/usr/bin/env bash
 
+# Using gmv from coreutils for backup feature.
+# Must come after brew.sh (installs coreutils).
+MV_COMMAND="/usr/local/bin/gmv"
+THIS_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+DOTFILES=("bash_profile" "cvimrc" "duti" "git/gitconfig" "git/gitignore_global" "ideavimrc" "ssh" "vim" "vim/vimrc")
+
 printf "Symlinking dotfiles to ${HOME}...\n\n"
 
-THIS_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
-dotfiles=("bash_profile" "cvimrc" "duti" "git/gitconfig" "git/gitignore_global" "ideavimrc" "ssh" "vim" "vim/vimrc")
-
-for dotfilePath in "${dotfiles[@]}"
+for dotfilepath in "${DOTFILES[@]}"
 do
-  dotfileName=$(basename "${dotfilePath}")
+  dotfilename=$(basename "${dotfilepath}")
+  src="${THIS_DIR}/${dotfilepath}"
+  dst="${HOME}/.${dotfilename}"
 
-  if [[ ! -f ${THIS_DIR}/${dotfilePath} && ! -d ${THIS_DIR}/${dotfilePath} ]]; then
-    printf "WARNING: ${THIS_DIR}/${dotfilePath} doesn't exist, skipping...\n"
+  if [[ ! -f ${src} && ! -d ${src} ]]; then
+    printf "WARNING: ${src} doesn't exist, skipping...\n"
     continue
   fi
 
   # Back up existing dotfiles before replacing if they're not symlinks.
-  if [[ ( -f ${HOME}/.${dotfileName} || -d ${HOME}/.${dotfileName} ) && ! -L ${HOME}/.${dotfileName} ]]; then
-    printf "${HOME}/.${dotfileName} already exists, backing up before overwriting...\n"
-    # Using gmv from coreutils for backup feature.
-    # Must come after brew.sh (installs coreutils).
-    /usr/local/bin/gmv --backup=numbered "${HOME}/.${dotfileName}" "${HOME}/.${dotfileName}.backup"
-    printf "Finished backing up ${HOME}/.${dotfileName}.\n"
+  if [[ ( -f ${dst} || -d ${dst} ) && ! -L ${dst} ]]; then
+    printf "${dst} already exists, backing up before overwriting...\n"
+    if command -v ${MV_COMMAND} &>/dev/null; then
+      ${MV_COMMAND} --backup=numbered "${dst}" "${dst}.backup"
+    else
+      printf "WARNING: Can't find ${MV_COMMAND} for making backups. Skipping ${dst}...\n"
+      continue
+    fi
+    printf "Finished backing up ${dst}.\n"
   fi
 
-  printf "Symlinking ${THIS_DIR}/${dotfilePath} as ${HOME}/.${dotfileName}\n"
-  ln -sfn ${THIS_DIR}/${dotfilePath} "${HOME}/.${dotfileName}"
+  printf "Symlinking ${src} as ${dst}\n"
+  ln -sfn ${src} "${dst}"
 done
 
 printf "\nFinished symlinking dotfiles.\n\n"
