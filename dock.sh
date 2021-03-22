bef89d671afe11ac9cbaff8ab59eb8c76e3804a1
Aaron Mahan <knightofiam@gmail.com>
2021-03-22 12:26:52 -0600

    Update dock.

--- dock.sh
+++ dock.sh
@@ -7,27 +7,45 @@ dockutil --add "/Applications/iTerm.app"
 dockutil --add "/Applications/MacVim.app"
 dockutil --add "/Applications/Rider.app"
 dockutil --add "/Applications/Godot_mono.app"
+dockutil --add "/Applications/LMMS.app"
 dockutil --add "/Applications/Discord.app"
-dockutil --add "/Applications/Slack.app"
+dockutil --add "/Applications/VMware Fusion.app"
 dockutil --add "/Applications/zoom.us.app"
-dockutil --add "/Applications/VirtualBox.app"
+dockutil --add "/Applications/Slack.app"
 dockutil --add "/System/Applications/Messages.app"
 dockutil --add "/System/Applications/FaceTime.app"
 dockutil --add "/Applications/Signal.app"
-dockutil --add "/System/Applications/Calendar.app"
+dockutil --add "/Applications/Pages.app"
+dockutil --add "/Applications/GarageBand.app"
+dockutil --add "/Applications/Airfoil.app"
 dockutil --add "/System/Applications/Notes.app"
+dockutil --add "/System/Applications/Calendar.app"
+dockutil --add "/System/Applications/Utilities/Screenshot.app"
 dockutil --add "/System/Applications/Calculator.app"
 dockutil --add "/Applications/1Password 7.app"
+dockutil --add "/Applications/Speedtest.app"
 dockutil --add "/System/Applications/Utilities/Activity Monitor.app"
-dockutil --add "/System/Applications/System Preferences.app"
 dockutil --add "/System/Applications/System Preferences.app"
 dockutil --add '~/Downloads' --view grid --display folder
 dockutil --add '~/Sync/secureset/secureset-notes.txt'
-dockutil --add '~/Sync/secureset/net/secureset-net-notes.txt'
-dockutil --add '~/Sync/secureset/sys/secureset-sys-notes.txt'
 dockutil --add '~/Sync/dev/dotfiles-todo.txt'
 dockutil --add '~/Sync/dev/coa/notes/coa-todo.txt'
 dockutil --add '~/Sync/dev/vim-tips.txt'
 dockutil --add '~/Sync/dev/git-tips.txt'
 dockutil --add '~/Sync/dev/iterm2-tips.txt'
 dockutil --add '~/Sync/dev/godot-tips.txt'
+dockutil --add https://flatironschool.zoom.us/j/95936609425 --label 'SecureSet Zoom'
+dockutil --add https://learning.flatironschool.com/courses/2514 --label 'SecureSet Canvas Homeroom'
+dockutil --add https://learning.flatironschool.com/courses/2511/modules --label 'SecureSet Cryptography'
+dockutil --add https://learning.flatironschool.com/courses/3141/modules --label 'SecureSet Networking'
+dockutil --add https://learning.flatironschool.com/courses/2518/modules --label 'SecureSet Systems'
+dockutil --add https://learning.flatironschool.com/courses/2519/modules --label 'SecureSet Threat'
+dockutil --add https://learning.flatironschool.com/courses/2513/modules --label 'SecureSet GRC'
+dockutil --add https://portal.flatironschool.com/coaching/journey --label 'SecureSet Career Coaching'
+dockutil --add https://github.com/knightofiam --label 'GitHub: knightofiam'
+dockutil --add https://github.com/forerunnergames --label 'GitHub: forerunnergames'
+dockutil --add https://github.com/knightofiam/dotfiles --label 'GitHub: dotfiles'
+dockutil --add https://github.com/forerunnergames/coa --label 'GitHub: coa'
+dockutil --add https://downloads.tuxfamily.org/godotengine --label 'Godot Downloads'
+dockutil --add https://console.aws.amazon.com --label 'AWS'
+dockutil --add https://cs.smu.ca/~porter/csc/ref/ascii.html --label 'ASCII Table'
