83d882f021a1020a692020ae06180cf444e904db
Aaron Mahan <knightofiam@gmail.com>
2021-03-24 14:52:41 -0600

    Improve comments in iterm2.sh.
    - Update to match their corresponding comments in bash_profile.

--- iterm2.sh
+++ iterm2.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-# Enable iTerm2 shell integration for searching command history.
-# (See https://iterm2.com/documentation-shell-integration.html)
+# Install iTerm2 command history & many other featuers
+# https://iterm2.com/documentation-shell-integration.html
 curl -L https://iterm2.com/shell_integration/install_shell_integration.sh | bash
