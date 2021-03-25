f22ce1b625e746c55d11785c49f3cf130f704c1e
Aaron Mahan <knightofiam@gmail.com>
2021-03-25 10:09:27 -0600

    Update mvim for MacVim 8.2.2576 (170).
    - Reapply tab bugfix in updated mvim script.
    - Improve comments in mvim & mvim.sh for consistency & clarity.

--- mvim.sh
+++ mvim.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-# Modify mvim to open new tabs instead of new windows
-# https://stackoverflow.com/a/9100989/14410393
+# Modify mvim to open files in new tabs, not new windows.
+# See: https://stackoverflow.com/a/9100989/14410393
 cp vim/mvim /usr/local/bin/mvim
