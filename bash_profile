knightofiam/dotfiles

commit ddf4709e84ebdbdc9ba7a4132dc283f464a71f06

Fix linux bugs.
- Don't assume Homebrew path; use 'brew --prefix'.
- Run conditional checks on everything in bash_profile to avoid errors.

bash_profile
@@ -1,47 +1,36 @@
 # Homebrew
-export PATH="/usr/local/bin:${PATH}"
-export PATH="/usr/local/sbin:${PATH}"
-
-# Ruby
-export PATH="/usr/local/opt/ruby/bin:${PATH}"
-export PATH="/usr/local/lib/ruby/gems/2.7.0/bin:${PATH}"
+[[ $(which brew) ]] &&\
+  export PATH="$(brew --prefix)/bin:$(brew --prefix)/sbin:${PATH}"
 
 # Butler
-export PATH="$PATH:$HOME/Library/Application Support/itch/broth/butler/versions/15.20.0"
+[[ $(which butler) ]] &&\
+  export PATH="$PATH:$HOME/Library/Application Support/itch/broth/butler/versions/15.20.0"
 
 # Bash completion
-if [ -f $(brew --prefix)/etc/bash_completion ]; then
-  source $(brew --prefix)/etc/bash_completion
-fi
+[[ $(which brew) && -f $(brew --prefix)/etc/bash_completion ]] &&\
+  . $(brew --prefix)/etc/bash_completion
 
 # Git completion
-if [ -f $(brew --prefix)/etc/bash_completion.d/git-completion.bash ]; then
-  source $(brew --prefix)/etc/bash_completion.d/git-completion.bash
-fi
+[[ $(which brew) && -f $(brew --prefix)/etc/bash_completion.d/git-completion.bash ]] &&\
+  . $(brew --prefix)/etc/bash_completion.d/git-completion.bash
 
 # iTerm2 command history & many other featuers
 # https://iterm2.com/documentation-shell-integration.html
-if [ -e ~/.iterm2_shell_integration.bash ]; then
-  source ~/.iterm2_shell_integration.bash
-fi
+[[ -e ~/.iterm2_shell_integration.bash ]] && . ~/.iterm2_shell_integration.bash
 
 # Python
-if test $(which pyenv); then
-  eval "$(pyenv init -)"
-fi
+[[ $(which pyenv) ]] && eval "$(pyenv init -)"
 
 # Disable CTRL+D 10 times before quitting.
 # Prevents accidentally closing the terminal.
 set -o ignoreeof
 
 # Pandora
-source ~/.pandora
+[[ -f ~/.pandora ]] && . ~/.pandora
 
 # Aliases
-source ~/.bash_aliases
+[[ -f ~/.bash_aliases ]] && . ~/.bash_aliases
 
 # Private environment variables.
 # Use set -a to export all variables without explicitly using 'export'.
-set -a
-source ~/.extra
-set +a
+[[ -f ~/.extra ]] && { set -a; . ~/.extra; set +a; }

gpg.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Using gmv from coreutils for backup feature.
+# Must come after brew.sh (installs coreutils).
+MV_COMMAND="$(brew --prefix)/bin/gmv"
+
 # Sign into 1Password
 eval $(op signin my.1password.com ${ONE_PASSWORD_EMAIL})
 
@@ -7,9 +11,7 @@ eval $(op signin my.1password.com ${ONE_PASSWORD_EMAIL})
 # op refuses to overwrite.
 if [[ -f ${HOME}/key.asc ]]; then
   printf "\n${HOME}/key.asc already exists, backing up before overwriting...\n"
-  # Using gmv from coreutils for backup feature.
-  # Must come after `brew bundle` (installs coreutils).
-  /usr/local/bin/gmv --backup=numbered "${HOME}/key.asc" "${HOME}/key.asc.backup"
+  ${MV_COMMAND} --backup=numbered "${HOME}/key.asc" "${HOME}/key.asc.backup"
   printf "\nFinished backing up ${HOME}/key.asc.\n\n"
 fi
 

symlink.sh
@@ -2,7 +2,7 @@
 
 # Using gmv from coreutils for backup feature.
 # Must come after brew.sh (installs coreutils).
-MV_COMMAND="/usr/local/bin/gmv"
+MV_COMMAND="$(brew --prefix)/bin/gmv"
 THIS_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 
 printf "Symlinking dotfiles to ${HOME}...\n\n"

commit d7bade9bc7d9b160ca702e4c61c26ccd6e0273e4

Fix bugs in bash_profile.
- Fix circular logic regressions caused by ddf4709, where a command such
  as 'brew' will not be added to the path because it "doesn't exist"
  because it hasn't been added to the path yet. This fix results in
  needing to use a fixed path for Homebrew (only works on macOS out of
  the box).

- Clean up by using more variables for succinctness and to avoid
  duplication.

bash_profile
@@ -1,29 +1,25 @@
 # Homebrew
-[[ $(which brew) ]] &&\
-  export PATH="$(brew --prefix)/bin:$(brew --prefix)/sbin:${PATH}"
+brew_prefix="/usr/local"
+[[ -f ${brew_prefix}/bin/brew ]] && export PATH="${brew_prefix}/bin:${brew_prefix}/sbin:${PATH}"
 
 # Butler
-[[ $(which butler) ]] &&\
-  export PATH="$PATH:$HOME/Library/Application Support/itch/broth/butler/versions/15.20.0"
+butler_path="$HOME/Library/Application Support/itch/broth/butler/versions/15.20.0"
+[[ -d ${butler_path} ]] && export PATH="$PATH:${butler_path}"
 
 # Bash completion
-[[ $(which brew) && -f $(brew --prefix)/etc/bash_completion ]] &&\
-  . $(brew --prefix)/etc/bash_completion
+bash_completion="${brew_prefix}/etc/bash_completion"
+[[ -f ${bash_completion} ]] && . ${bash_completion}
 
 # Git completion
-[[ $(which brew) && -f $(brew --prefix)/etc/bash_completion.d/git-completion.bash ]] &&\
-  . $(brew --prefix)/etc/bash_completion.d/git-completion.bash
+git_completion="${brew_prefix}/etc/bash_completion.d/git-completion.bash"
+[[ -f ${git_completion} ]] && . ${git_completion}
 
 # iTerm2 command history & many other featuers
 # https://iterm2.com/documentation-shell-integration.html
 [[ -e ~/.iterm2_shell_integration.bash ]] && . ~/.iterm2_shell_integration.bash
 
 # Python
-[[ $(which pyenv) ]] && eval "$(pyenv init -)"
-
-# Disable CTRL+D 10 times before quitting.
-# Prevents accidentally closing the terminal.
-set -o ignoreeof
+$(command -v pyenv &>/dev/null) && eval “$(pyenv init -)”
 
 # Pandora
 [[ -f ~/.pandora ]] && . ~/.pandora
@@ -34,3 +30,7 @@ set -o ignoreeof
 # Private environment variables.
 # Use set -a to export all variables without explicitly using 'export'.
 [[ -f ~/.extra ]] && { set -a; . ~/.extra; set +a; }
+
+# Disable CTRL+D 10 times before quitting.
+# Prevents accidentally closing the terminal.
+set -o ignoreeof