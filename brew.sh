8d5bbe80f7d5d36397e72f64ad9fe00fd827d13c
Aaron Mahan <knightofiam@gmail.com>
2021-04-11 16:29:18 -0600

    Fix homebrew post-install message formatting.

--- brew.sh
+++ brew.sh
@@ -4,7 +4,7 @@ printf "Checking if Homebrew is installed...\n\n"
 if test ! $(which brew); then
   printf "Installing Homebrew...\n\n"
   echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  printf "\nFinished installing Homebrew.\n\n"
+  printf "Finished installing Homebrew.\n\n"
 else
   printf "Homebrew is already installed.\n\n"
 fi
