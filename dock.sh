283f20564c0619675477e0a675f554c3c7f09be1
First Last <first.last@example.com>
2021-07-20 13:54:50 -0600

    Update dock.
    - Update file paths in Sync.
    - Add LearnKey URL.

--- dock.sh
+++ dock.sh
@@ -28,12 +28,14 @@ dockutil --add "/System/Applications/Utilities/Activity Monitor.app"
 dockutil --add "/System/Applications/Preview.app"
 dockutil --add "/System/Applications/System Preferences.app"
 dockutil --add '~/Downloads' --view grid --display folder
-dockutil --add '~/Sync/dev/dotfiles-todo.txt'
+dockutil --add '~/Sync/career/sec-plus-notes.txt'
+dockutil --add '~/Sync/dev/dotfiles/dotfiles-todo.txt'
 dockutil --add '~/Sync/dev/coa/notes/coa-todo.txt'
-dockutil --add '~/Sync/dev/vim-tips.txt'
-dockutil --add '~/Sync/dev/git-tips.txt'
-dockutil --add '~/Sync/dev/iterm2-tips.txt'
-dockutil --add '~/Sync/dev/godot-tips.txt'
+dockutil --add '~/Sync/dev/vim/vim-tips.txt'
+dockutil --add '~/Sync/dev/git/git-tips.txt'
+dockutil --add '~/Sync/dev/iterm/iterm2-tips.txt'
+dockutil --add '~/Sync/dev/godot/godot-tips.txt'
+dockutil --add https://lms.onlineexpert.com/ --label 'LearnKey'
 dockutil --add https://github.com/knightofiam --label 'GitHub: knightofiam'
 dockutil --add https://github.com/forerunnergames --label 'GitHub: forerunnergames'
 dockutil --add https://github.com/knightofiam/dotfiles --label 'GitHub: dotfiles'
