c6dbb5842692ddaa2d4e4958657d6f09485d8fb9
Aaron Mahan <knightofiam@gmail.com>
2021-08-18 16:02:07 -0600

    Update screen corner functionality.
    - Use top right corner for hiding the mouse, so do nothing.
    - Use bottom right corner for showing the desktop.

--- macos.sh
+++ macos.sh
@@ -345,15 +345,18 @@ defaults write com.apple.dock showLaunchpadGestureEnabled -int 0
 # 11: Launchpad
 # 12: Notification Center
 # 13: Lock Screen
-# Top left screen corner → Mission Control
+# Top left screen corner - Mission Control
 defaults write com.apple.dock wvous-tl-corner -int 2
 defaults write com.apple.dock wvous-tl-modifier -int 0
-# Top right screen corner → Desktop
-defaults write com.apple.dock wvous-tr-corner -int 4
-defaults write com.apple.dock wvous-tr-modifier -int 0
-# Bottom left screen corner → Start screen saver
+# Bottom right screen corner - Show desktop
+defaults write com.apple.dock wvous-br-corner -int 4
+defaults write com.apple.dock wvous-br-modifier -int 0
+# Bottom left screen corner - Start screen saver
 defaults write com.apple.dock wvous-bl-corner -int 5
 defaults write com.apple.dock wvous-bl-modifier -int 0
+# Top right screen corner - reserved for hiding the mouse
+defaults write com.apple.dock wvous-tr-corner -int 0
+defaults write com.apple.dock wvous-tr-modifier -int 0
 
 # ============================================================================
 # Begin Safari Section
