11a045872531b1a42524937cffda3277d71e21b3
Aaron Mahan <knightofiam@gmail.com>
2022-04-30 23:47:02 -0700

    Update Xcode CLT installation.
    - Accept license on the command line after installing / configuring CLT.

--- xcode.sh
+++ xcode.sh
@@ -5,5 +5,6 @@ if [[ ! -d "$('xcode-select' -print-path 2>/dev/null)" ]]; then
   printf "Configuring Xcode CLT...\n\n"
   sudo xcode-select --install
   sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
+  sudo xcodebuild -license accept
   printf "\nFinished configuring Xcode CLT.\n\n"
 fi
