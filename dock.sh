143303b686628b0ec11b4e7b1e7e24f0b254aa4e
Aaron Mahan <knightofiam@gmail.com>
2022-09-10 14:57:58 -0600

    Update Dock.

--- dock.sh
+++ dock.sh
@@ -8,42 +8,43 @@ dockutil --add "/Applications/MacVim.app"
 dockutil --add "/Applications/Rider.app"
 dockutil --add "/Applications/Godot_mono.app"
 dockutil --add "~/Library/Application Support/Steam/steamapps/common/Aseprite/Aseprite.app"
-dockutil --add "/Applications/PyxelEdit.app"
-dockutil --add "/Applications/LMMS.app"
+dockutil --add "/Applications/Tilesetter.app"
 dockutil --add "/Applications/Discord.app"
 dockutil --add "/Applications/VMware Fusion.app"
 dockutil --add "/Applications/zoom.us.app"
-dockutil --add "/Applications/Slack.app"
 dockutil --add "/System/Applications/Messages.app"
 dockutil --add "/System/Applications/FaceTime.app"
 dockutil --add "/Applications/Signal.app"
 dockutil --add "/Applications/Pages.app"
 dockutil --add "/Applications/GarageBand.app"
-dockutil --add "/Applications/Airfoil.app"
 dockutil --add "/System/Applications/Notes.app"
 dockutil --add "/System/Applications/Calendar.app"
 dockutil --add "/System/Applications/Utilities/Screenshot.app"
 dockutil --add "/System/Applications/Calculator.app"
-dockutil --add "/Applications/1Password 7.app"
+dockutil --add "/Applications/1Password.app"
 dockutil --add "/System/Applications/Utilities/Activity Monitor.app"
 dockutil --add "/System/Applications/Preview.app"
 dockutil --add "/System/Applications/QuickTime Player.app"
 dockutil --add "/System/Applications/System Preferences.app"
 dockutil --add '~/Downloads' --view grid --display folder
+dockutil --add '~/Sync/career/hack-the-box.txt'
 dockutil --add '~/Sync/dev/dotfiles/dotfiles-todo.txt'
 dockutil --add '~/Sync/dev/coa/notes/coa-todo.txt'
 dockutil --add '~/Sync/dev/vim/vim-tips.txt'
 dockutil --add '~/Sync/dev/git/git-tips.txt'
+dockutil --add '~/Sync/dev/rider/rider-tips.txt'
 dockutil --add '~/Sync/dev/iterm/iterm2-tips.txt'
 dockutil --add '~/Sync/dev/godot/godot-tips.txt'
 dockutil --add '~/Sync/dev/aseprite/aseprite-tips.txt'
 dockutil --add '~/Sync/dev/garageband/garageband-tips.txt'
+dockutil --add https://app.hackthebox.com/home --label 'Hack The Box'
+dockutil --add https://console.aws.amazon.com --label 'AWS'
 dockutil --add https://www.lumosity.com/app/v4/dashboard --label 'Lumosity'
+dockutil --add https://jstris.jezevec10.com --label 'Jstris'
 dockutil --add https://github.com/knightofiam --label 'GitHub: knightofiam'
 dockutil --add https://github.com/forerunnergames --label 'GitHub: forerunnergames'
 dockutil --add https://github.com/knightofiam/dotfiles --label 'GitHub: dotfiles'
 dockutil --add https://github.com/forerunnergames/coa --label 'GitHub: coa'
 dockutil --add https://forerunnergames.itch.io/coa --label 'itch.io: coa'
 dockutil --add https://downloads.tuxfamily.org/godotengine --label 'Godot Downloads'
-dockutil --add https://console.aws.amazon.com --label 'AWS'
 dockutil --add https://cs.smu.ca/~porter/csc/ref/ascii.html --label 'ASCII Table'
