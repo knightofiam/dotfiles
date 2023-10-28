553d827609a664564cc2bb135ab8fd633540b5f1
Aaron Mahan <aaron@forerunnergames.com>
2023-10-28 06:05:18 -0700

    Install crypto support in python.

--- python.sh
+++ python.sh
@@ -3,3 +3,5 @@
 # Python 2 no longer exists on MacOS by default, causing problems in some apps
 # such as ebcli. Symlink 'python' command to 'python3'.
 sudo ln -sf /usr/local/bin/python3 /usr/local/bin/python
+
+pip install cryptography
